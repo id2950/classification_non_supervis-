8c8906b582dd7babe3fc532eb81e2f7e64ec6cdb
raphael torreilles <raphael.torreilles@eleve.ensai.fr>
2026-04-03 12:56:50 +0000

    dd

---
+++ TP1.R
@@ -0,0 +1,16 @@
+Fonction_echantillon<-function(n,delta){
+  moyennes<-matrix(c(-delta,-delta,delta,-delta,-delta,delta,delta,delta),nrow = 4,ncol = 2,byrow = TRUE)
+  n_e<-4
+  ech<-list()
+  for (i in 1:n_e) {
+    ech[[i]]<-data.frame(x1=rnorm(n,mean=moyennes[i,1],sd=1),
+                         x2=rnorm(n,mean=moyennes[i,2],sd=1),
+                         P=i)
+  }
+  echantillon<-rbind(ech[[1]],ech[[2]],
+                     ech[[3]],ech[[4]])
+  echantillon$P <- factor(echantillon$P)
+  return(echantillon)
+}
+
+echantillon_El = Fonction_echantillon(5,3)
